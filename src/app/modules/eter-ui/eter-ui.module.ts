50c536712a689396cb135216c7bac7ef1566be9c
Felipe Mantilla <felipemantilla@outlook.es>
2020-05-18 12:02:36 -0500

    instalacion de angular material

---
+++ src/app/modules/eter-ui/eter-ui.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+/** @material modules */
+
+/** end @material mdoules */
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class EterUiModule { }
