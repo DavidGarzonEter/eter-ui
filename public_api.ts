2532e9c6c2d1d53eec9e048e1257a38eb353953a
edgarzon93 <65820725+edgarzon93@users.noreply.github.com>
2020-07-02 16:14:25 -0500

    inclucion interface combos

--- public_api.ts
+++ public_api.ts
@@ -4,5 +4,6 @@ export * from './src/app/modules/services/crypto.service';
 export * from './src/app/modules/services/message.service';
 export * from './src/app/modules/services/session.service';
 export * from './src/app/modules/services/http.service';
-export * from './src/app/modules/interfaces/data-table/table-columns'
-export * from './src/app/modules/interfaces/data-table/table-configuration'
+export * from './src/app/modules/interfaces/data-table/table-columns';
+export * from './src/app/modules/interfaces/data-table/table-configuration';
+export * from './src/app/modules/interfaces/combos/combos-configuration';
